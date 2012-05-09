e7faa8327bff752fed8bd3c7c24cfe3ffdf86c78
rikiishi <riki0000qwe@gmail.com>
2012-05-10 08:07:21 +0900

    レンタルサーバ上で変更してみた

--- webApp/index.php
+++ webApp/index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html lang="ja">
 <head>
+
 	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 	<title>webApp</title>
 	<link rel="stylesheet" href="style.css" />
